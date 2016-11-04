598baf1f964fcd29fc07a243797b0300c542bbe1
Billard <sirfrancisbillard@gmail.com>
2016-11-04 13:22:58 -1000

    Create citizens.lua

---
+++ lua/darkrp_customthings/jobs/citizens.lua
@@ -0,0 +1,67 @@
+TEAM_CITIZEN = DarkRP.createJob("Citizen", {
+    color = Color(20, 150, 20, 255),
+    model = {
+        "models/player/Group01/Female_01.mdl",
+        "models/player/Group01/Female_02.mdl",
+        "models/player/Group01/Female_03.mdl",
+        "models/player/Group01/Female_04.mdl",
+        "models/player/Group01/Female_06.mdl",
+        "models/player/group01/male_01.mdl",
+        "models/player/Group01/Male_02.mdl",
+        "models/player/Group01/male_03.mdl",
+        "models/player/Group01/Male_04.mdl",
+        "models/player/Group01/Male_05.mdl",
+        "models/player/Group01/Male_06.mdl",
+        "models/player/Group01/Male_07.mdl",
+        "models/player/Group01/Male_08.mdl",
+        "models/player/Group01/Male_09.mdl"
+    },
+    description = [[The Citizen is the most basic level of society you can hold besides being a hobo. You have no specific role in city life.]],
+    weapons = {},
+    command = "citizen",
+    max = 0,
+    salary = GAMEMODE.Config.normalsalary,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Citizens",
+})
+
+TEAM_MEDIC = DarkRP.createJob("Medic", {
+    color = Color(47, 79, 79, 255),
+    model = "models/player/kleiner.mdl",
+    description = [[With your medical knowledge you work to restore players to full health.
+        Without a medic, people cannot be healed.
+        Left click with the Medical Kit to heal other players.
+        Right click with the Medical Kit to heal yourself.]],
+    weapons = {"med_kit"},
+    command = "medic",
+    max = 3,
+    salary = GAMEMODE.Config.normalsalary,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    medic = true,
+    category = "Citizens",
+})
+
+TEAM_HOBO = DarkRP.createJob("Hobo", {
+    color = Color(80, 45, 0, 255),
+    model = "models/player/corpse1.mdl",
+    description = [[The lowest member of society. Everybody laughs at you.
+        You have no home.
+        Beg for your food and money
+        Sing for everyone who passes to get money
+        Make your own wooden home somewhere in a corner or outside someone else's door]],
+    weapons = {"weapon_bugbait"},
+    command = "hobo",
+    max = 5,
+    salary = 0,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    hobo = true,
+    category = "Citizens",
+})
